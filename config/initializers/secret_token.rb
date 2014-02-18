9657388aa4d8b10fc35da392ff245db328e032a4
robw <robw@Archaeopteryx.(none)>
2014-02-17 16:21:27 -0800

    User authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth::Application.config.secret_key_base = '18f752118213706e1d488870bfd9ba5695b6ca5a082355cb0c1412c64fce570b2c304c2ea89fd16563d20141a53d1bc0b231bbce174a6c07f630f408b80c4019'
